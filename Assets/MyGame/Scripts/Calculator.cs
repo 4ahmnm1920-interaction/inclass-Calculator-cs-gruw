55aa83c25e63f303b0ae772d40bedb4869471350
smeerws <wilfried.gruber@htl-salzburg.ac.at>
2020-01-15 10:58:21 +0100

    Add calculator Script

---
+++ Assets/MyGame/Scripts/Calculator.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Calculator : MonoBehaviour
+{
+    public InputField number1;
+    public InputField number2;
+    public Text operatorText;
+    public Text resultText;
+
+    public void AddNumbers()
+    {
+        operatorText.text = "+";
+        float result = float.Parse(number1.text) + float.Parse(number2.text);
+        Debug.Log(result);
+        resultText.text = result.ToString();
+    }
+
+    public void SubtractNumbers()
+    {
+        operatorText.text = "-";
+        float result = float.Parse(number1.text) - float.Parse(number2.text);
+        Debug.Log(result);
+        resultText.text = result.ToString();
+    }
+
+    public void MultiplyNumbers()
+    {
+        operatorText.text = "*";
+        float result = float.Parse(number1.text) * float.Parse(number2.text);
+        Debug.Log(result);
+        resultText.text = result.ToString();
+    }
+
+    public void DivideNumbers()
+    {
+        operatorText.text = "/";
+        float result = float.Parse(number1.text) / float.Parse(number2.text);
+        Debug.Log(result);
+        resultText.text = result.ToString();
+    }
+}
