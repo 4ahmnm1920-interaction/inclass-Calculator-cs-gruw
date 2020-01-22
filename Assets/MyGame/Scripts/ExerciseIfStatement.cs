cdb10fcc8ebaefdbee7210b7d0b5258545ce1d68
GruberWilfried <wilfried.gruber@htl-salzburg.ac.at>
2020-01-22 11:38:20 +0100

    Add Exercise Scripts

---
+++ Assets/MyGame/Scripts/ExerciseIfStatement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExerciseIfStatement : MonoBehaviour
+{
+    public bool condition;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (condition)
+        {
+            Debug.Log("Diese Aussage(Condition) ist richtig!");
+        }
+        else
+        {
+            Debug.Log("Diese Aussage(Condition) ist falsch!");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/MyGame/Scripts/ExerciseReturnValue.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExerciseReturnValue : MonoBehaviour
+{
+    public float value1;
+    public float value2;
+
+    void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        Debug.Log(Random.Range(value1,value2) + Random.Range(value1,value2));
+    }
+
+    public int AddIntegers(int variable1, int variable2)
+    {
+        return (variable1 + variable2);
+    }
+
+    /*
+    public int AddNumbers()
+    {
+        
+    }
+    */
+    /*
+    public float FloatReturnValue()
+    {
+
+    }
+
+    public string StringReturnValue()
+    {
+
+    }
+    */
+}
